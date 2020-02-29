46975d6e9664e8b128c1ffc3b4c367a5e73437aa
WildDisk <ivan.zolotopup@gmail.com>
2020-03-01 09:49:31 +1000

    add base projcet

---
+++ src/main/kotlin/com/example/SpringSarafan/SpringSarafanApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SpringSarafan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSarafanApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSarafanApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/SpringSarafan/SpringSarafanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SpringSarafan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSarafanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
